3bf885f03a66d18121c66aaed9c677b2a7d6e82c
Confidence Okere <okerenomso@gmail.com>
2022-01-26 04:27:36 +0100

    Still on Hostel Management system

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import _ "hostelManagement/hostelManagement"
 
 func main() {
 	var room [5]bool
-	//var bed [5]bool
-	//var bedInput int
+	var bed [5]bool
+	var bedInput int
 	var roomInput int
 
 	firstStudent := hostelManagement.NewStudent("Ada", 20, 2)
@@ -24,14 +24,14 @@ func main() {
 		hostelManagement.AssignRoom(roomGenerated, roomInput, room)
 	}
 
-	//fmt.Println("Choose a bed number between 1 - 5:")
-	//fmt.Scanf("%d", bedInput)
-	//
-	//bedGenerated := hostelManagement.GenerateBedSpaceNumbers(bed)
-	//
-	//if bedInput >= 1 && bedInput <= 5{
-	//	hostelManagement.GenerateBedSpaceNumbers(bedGenerated, bedInput, bed )
-	//}else{
-	//	fmt.Println("We're Out of beds!")
-	//}
+	fmt.Println("Choose a bed number between 1 - 5:")
+	fmt.Scanf("%d", bedInput)
+
+	bedGenerated := hostelManagement.GenerateBedSpaceNumbers(bed)
+
+	if bedInput >= 1 && bedInput <= 5 {
+		hostelManagement.GenerateBedSpaceNumbers(bedGenerated, bedInput, bed)
+	} else {
+		fmt.Println("We're Out of beds!")
+	}
 }
